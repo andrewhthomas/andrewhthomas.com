fc5739dfa93707ea1d5795f34473dd9ff36de0e1
Andrew Thomas <andrew@messageagency.com>
2022-02-21 23:23:45 -0500

    Tailwind config

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -2,11 +2,18 @@ module.exports = {
   content: ['_site/**/*.html'],
   safelist: [],
   theme: {
+    fontFamily: {
+    'sans': ['acumin-pro-1', 'acumin-pro-2'],
+    'display': ['ivypresto-display-1', 'ivypresto-display-2'],
+    },
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         change: 'black',
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [ require("@tailwindcss/typography"), require("daisyui")],
 }
